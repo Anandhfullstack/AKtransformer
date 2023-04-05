8a408cac05f16fe41a90cb43a9ef2419e1bd9739
Anandhfullstack <ananthagopi003@gmail.com>
2023-04-06 01:33:20 +0800

    model.py is added

--- model.py
+++ model.py
@@ -1,99 +1,70 @@
-class PositionalEncoding(nn.Module):
-    """
-    compute sinusoid encoding.
-    """
-    def __init__(self, d_model, max_len, device):
-        """
-        constructor of sinusoid encoding class
 
-        :param d_model: dimension of model
-        :param max_len: max sequence length
-        :param device: hardware device setting
-        """
-        super(PositionalEncoding, self).__init__()
+import torch
+from torch import nn
 
-        # same size with input matrix (for adding with input matrix)
-        self.encoding = torch.zeros(max_len, d_model, device=device)
-        self.encoding.requires_grad = False  # we don't need to compute gradient
-
-        pos = torch.arange(0, max_len, device=device)
-        pos = pos.float().unsqueeze(dim=1)
-        # 1D => 2D unsqueeze to represent word's position
-
-        _2i = torch.arange(0, d_model, step=2, device=device).float()
-        # 'i' means index of d_model (e.g. embedding size = 50, 'i' = [0,50])
-        # "step=2" means 'i' multiplied with two (same with 2 * i)
-
-        self.encoding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / d_model)))
-        self.encoding[:, 1::2] = torch.cos(pos / (10000 ** (_2i / d_model)))
-        # compute positional encoding to consider positional information of words
-
-    def forward(self, x):
-        # self.encoding
-        # [max_len = 512, d_model = 512]
-
-        batch_size, seq_len = x.size()
-        # [batch_size = 128, seq_len = 30]
-
-        return self.encoding[:seq_len, :]
-        # [seq_len = 30, d_model = 512]
-        # it will add with tok_emb : [128, 30, 512]
+from decoder import Decoder
+from encoder import Encoder
 
 
-class MultiHeadAttention(nn.Module):
+class Transformer(nn.Module):
 
-    def __init__(self, d_model, n_head):
-        super(MultiHeadAttention, self).__init__()
-        self.n_head = n_head
-        self.attention = ScaleDotProductAttention()
-        self.w_q = nn.Linear(d_model, d_model)
-        self.w_k = nn.Linear(d_model, d_model)
-        self.w_v = nn.Linear(d_model, d_model)
-        self.w_concat = nn.Linear(d_model, d_model)
+    def __init__(self, src_pad_idx, trg_pad_idx, trg_sos_idx, enc_voc_size, dec_voc_size, d_model, n_head, max_len,
+                 ffn_hidden, n_layers, drop_prob, device):
+        super().__init__()
+        self.src_pad_idx = src_pad_idx
+        self.trg_pad_idx = trg_pad_idx
+        self.trg_sos_idx = trg_sos_idx
+        self.device = device
+        self.encoder = Encoder(d_model=d_model,
+                               n_head=n_head,
+                               max_len=max_len,
+                               ffn_hidden=ffn_hidden,
+                               enc_voc_size=enc_voc_size,
+                               drop_prob=drop_prob,
+                               n_layers=n_layers,
+                               device=device)
 
-    def forward(self, q, k, v, mask=None):
-        # 1. dot product with weight matrices
-        q, k, v = self.w_q(q), self.w_k(k), self.w_v(v)
+        self.decoder = Decoder(d_model=d_model,
+                               n_head=n_head,
+                               max_len=max_len,
+                               ffn_hidden=ffn_hidden,
+                               dec_voc_size=dec_voc_size,
+                               drop_prob=drop_prob,
+                               n_layers=n_layers,
+                               device=device)
 
-        # 2. split tensor by number of heads
-        q, k, v = self.split(q), self.split(k), self.split(v)
+    def forward(self, src, trg):
+        src_mask = self.make_pad_mask(src, src, self.src_pad_idx, self.src_pad_idx)
 
-        # 3. do scale dot product to compute similarity
-        out, attention = self.attention(q, k, v, mask=mask)
+        src_trg_mask = self.make_pad_mask(trg, src, self.trg_pad_idx, self.src_pad_idx)
 
-        # 4. concat and pass to linear layer
-        out = self.concat(out)
-        out = self.w_concat(out)
+        trg_mask = self.make_pad_mask(trg, trg, self.trg_pad_idx, self.trg_pad_idx) * \
+                   self.make_no_peak_mask(trg, trg)
 
-        # 5. visualize attention map
-        # TODO : we should implement visualization
+        enc_src = self.encoder(src, src_mask)
+        output = self.decoder(trg, enc_src, trg_mask, src_trg_mask)
+        return output
 
-        return out
+    def make_pad_mask(self, q, k, q_pad_idx, k_pad_idx):
+        len_q, len_k = q.size(1), k.size(1)
 
-    def split(self, tensor):
-        """
-        split tensor by number of head
+        # batch_size x 1 x 1 x len_k
+        k = k.ne(k_pad_idx).unsqueeze(1).unsqueeze(2)
+        # batch_size x 1 x len_q x len_k
+        k = k.repeat(1, 1, len_q, 1)
 
-        :param tensor: [batch_size, length, d_model]
-        :return: [batch_size, head, length, d_tensor]
-        """
-        batch_size, length, d_model = tensor.size()
+        # batch_size x 1 x len_q x 1
+        q = q.ne(q_pad_idx).unsqueeze(1).unsqueeze(3)
+        # batch_size x 1 x len_q x len_k
+        q = q.repeat(1, 1, 1, len_k)
 
-        d_tensor = d_model // self.n_head
-        tensor = tensor.view(batch_size, length, self.n_head, d_tensor).transpose(1, 2)
-        # it is similar with group convolution (split by number of heads)
+        mask = k & q
+        return mask
 
-        return tensor
+    def make_no_peak_mask(self, q, k):
+        len_q, len_k = q.size(1), k.size(1)
 
-    def concat(self, tensor):
-        """
-        inverse function of self.split(tensor : torch.Tensor)
+        # len_q x len_k
+        mask = torch.tril(torch.ones(len_q, len_k)).type(torch.BoolTensor).to(self.device)
 
-        :param tensor: [batch_size, head, length, d_tensor]
-        :return: [batch_size, length, d_model]
-        """
-        batch_size, head, length, d_tensor = tensor.size()
-        d_model = head * d_tensor
-
-        tensor = tensor.transpose(1, 2).contiguous().view(batch_size, length, d_model)
-        return tensor
+        return mask
